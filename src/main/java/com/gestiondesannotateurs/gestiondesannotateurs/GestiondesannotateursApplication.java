a124a071d94444e7b525b9bcace0c33614afc87a
Othman El hadrati <othmandone@gmail.com>
2025-04-23 18:49:41 +0100

    first one

---
+++ src/main/java/com/gestiondesannotateurs/gestiondesannotateurs/GestiondesannotateursApplication.java
@@ -0,0 +1,15 @@
+package com.gestiondesannotateurs.gestiondesannotateurs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestiondesannotateursApplication {
+
+	public static void main(String[] args) {
+
+		//test
+		SpringApplication.run(GestiondesannotateursApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestiondesannotateurs/gestiondesannotateurs/GestiondesannotateursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestiondesannotateurs.gestiondesannotateurs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestiondesannotateursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
